bb4139b1e554aa80c02f6be5cd91487d9d110bd2
Sergej Kalinin <agrotorgnn@gmail.com>
2025-12-12 15:23:19 +0300

    Initial PWA calculator

---
+++ servis-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'greenox-calculator-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/index.html');
+      })
+  );
+});
